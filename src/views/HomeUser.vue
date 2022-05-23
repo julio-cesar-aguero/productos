d59d64663981f3f08bc2dac5c682f5a7a44fe411
julio cesar aguero <julio.aguero.gomez@gmail.com>
2022-05-23 09:35:02 -0500

    lista de productos completa

--- src/views/HomeUser.vue
+++ src/views/HomeUser.vue
@@ -1,48 +1,36 @@
 <template>
-  <div class="container-fluid bg-primary d-flex w-100 p-5">
-    <div
-      class="container justify-content-start w-60 min-h-100 bg-info rounded m-2"
-    >
-      <h1 class="title m-2">Inicio Usuario</h1>
-
-      <div>
-        <b-table :items="items" :fields="fields" striped responsive="sm">
-          <template #cell(show_details)="row">
-            <b-button size="sm" @click="row.toggleDetails" class="mr-2">
-              {{ row.detailsShowing ? "Hide" : "Show" }} Details
-            </b-button>
-
-            <!-- As `row.showDetails` is one-way, we call the toggleDetails function on @change -->
-            <b-form-checkbox
-              v-model="row.detailsShowing"
-              @change="row.toggleDetails"
-            >
-              Details via check
-            </b-form-checkbox>
-          </template>
-
-          <template #row-details="row">
-            <b-card>
-              <b-row class="mb-2">
-                <b-col sm="3" class="text-sm-right"><b>Age:</b></b-col>
-                <b-col>{{ row.item.age }}</b-col>
-              </b-row>
-
-              <b-row class="mb-2">
-                <b-col sm="3" class="text-sm-right"><b>Is Active:</b></b-col>
-                <b-col>{{ row.item.isActive }}</b-col>
-              </b-row>
-
-              <b-button size="sm" @click="row.toggleDetails"
-                >Hide Details</b-button
+  <div class="container-fluid bg-primary d-flex col-14 pt-5">
+    <div class="container justify-content-around col-9 bg-info rounded">
+      <h1 class="title m-3 p-5">Inicio Usuario</h1>
+      <b-container class="bv-example-row">
+        <b-row cols="1" cols-md="2" cols-lg="3">
+          <b-col v-for="(item, index) of productos" :key="index">
+            <div class="w-100 producto p-1">
+              <b-card
+                :title="item.name"
+                img-src="https://picsum.photos/600/300/?image=25"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 25rem"
+                class="w-100"
               >
-            </b-card>
-          </template>
-        </b-table>
-      </div>
+                <b-card-text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </b-card-text>
+
+                <b-button href="#" variant="primary"
+                  >Agregar al carrito</b-button
+                >
+              </b-card>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
     </div>
 
-    <div class="carrito-area w-50 p-2 m-2 bg-secondary rounded">
+    <div class="carrito-area col-3 p-2 m-2 bg-secondary rounded h-60">
       <h2>Carrito</h2>
     </div>
   </div>
@@ -54,19 +42,13 @@ export default {
   data() {
     return {
       productos_local: [],
-       fields: ['first_name', 'last_name', 'show_details'],
-        items: [
-          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          {
-            isActive: false,
-            age: 89,
-            first_name: 'Geneva',
-            last_name: 'Wilson',
-            _showDetails: true
-          },
-          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
+      fields: ["first_name", "last_name", "show_details"],
+      items: [
+        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
+        { age: 21, first_name: "Larsen", last_name: "Shaw" },
+        { age: 89, first_name: "Geneva", last_name: "Wilson" },
+        { age: 38, first_name: "Jami", last_name: "Carney" },
+      ],
     };
   },
   computed: {
